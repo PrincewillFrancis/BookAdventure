c4ba325861e52ac9ae849dd7c03573d5116f3171
PrincewillFrancis <francis002@colum.edu>
2025-10-28 15:06:30 -0500

    finished chapter 6

---
+++ BookAdventure/Assets/Scripts/ItemRotation.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class ItemRotation : MonoBehaviour
+{
+
+    public int RotationSpeed = 100;
+    private Transform _itemTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _itemTransform = this.GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _itemTransform.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
+    }
+}
